e3bafe5c0d9567ef945d9768f735bdc526939daf
PratyushR <PratyushR@PratyushR01.emids.local>
2017-07-17 15:02:48 +0530

    Change

---
+++ src/test/java/org/totalbeginner/tutorial/PersonTest.java
@@ -0,0 +1,25 @@
+package org.totalbeginner.tutorial;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PersonTest {
+
+	@Test
+	//xyz
+	public void testPerson() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetMaximumBooks() {
+		fail("Not yet implemented");
+	}
+
+}
